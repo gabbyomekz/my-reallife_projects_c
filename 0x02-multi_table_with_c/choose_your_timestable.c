83146092aeb6d5fbd7e448fa7bab14a70331b8f6
Omeke Gabriel <gabbyomekz92@gmail.com>
2022-07-26 16:34:07 -0700

    A multiplication times table of your choice

---
+++ 0x02-multi_table_with_c/choose_your_timestable.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main -> Function declaration to perform multiplication
+ * @n: variable to hold the number of which multiplication
+ * Table is to be performed
+ * @i: variable for iteration up to 12 columns
+ * Return: void
+ */
+
+int main(void){
+int n, i;
+
+printf("Welcome to the Multiplication Times Table Dashboard\n");
+printf("Enter a number for the Multiplications table: ");
+scanf("%d", &n);
+
+for (i = 1; i <= 12; i++)
+{
+printf("%d * %d\t = %d\t\n", n, i, (n * i));
+}
+}
